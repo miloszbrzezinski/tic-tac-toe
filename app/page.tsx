a1ae39fe26d55f1ccded3fa3285304a144f5f321
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-03-20 23:40:18 +0100

    result calculating added

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     ["", "", ""],
   ]);
   const [sign, setSign] = useState("x");
+  const [result, setResult] = useState("tic tac toe");
 
   const makeMove = (row: number, col: number) => {
     let tmpBoard = [...board];
@@ -17,6 +18,7 @@ export default function Home() {
       tmpBoard[row][col] = sign;
       sign === "x" ? setSign("o") : setSign("x");
     }
+    setResult(isWinner());
     setBoard(tmpBoard);
   };
 
@@ -27,11 +29,44 @@ export default function Home() {
       ["", "", ""],
     ];
     setBoard(tmpBoard);
+    setResult("");
+  };
+
+  const isThreeSame = (first: string, second: string, third: string) => {
+    return first && first === second && second === third;
+  };
+
+  const isWinner = () => {
+    let winner = "";
+    board.forEach((row) => {
+      if (isThreeSame(row[0], row[1], row[2])) {
+        winner = row[0];
+      }
+    });
+    for (let i = 0; i < 3; i++) {
+      if (isThreeSame(board[0][i], board[1][i], board[2][i])) {
+        winner = board[0][i];
+      }
+    }
+    if (
+      isThreeSame(board[0][0], board[1][1], board[2][2]) ||
+      isThreeSame(board[0][2], board[1][1], board[2][0])
+    ) {
+      winner = board[1][1];
+    }
+
+    return winner;
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="space-y-10">
+        <div>
+          <p className="text-6xl font-light w-full text-center">Tic Tac Toe</p>
+          <p className="text-3xl">Player: {sign}</p>
+          <p className="text-3xl">Winner: {result}</p>
+        </div>
+
         <div className="grid grid-cols-3 bg-neutral-900 gap-1">
           {board.map((row, i) =>
             row.map((cell, j) => (
